274b49501fddb9159267cf3e2d5d8366aec1b9bb
mateoyamith <mateosanchez9602@gmail.com>
2021-01-27 07:15:24 -0500

    Desestructurar objetos

--- scripts/alcance.js
+++ scripts/alcance.js
@@ -3,7 +3,7 @@ var nombre2 = 'Mafe';
 var mateo = {
     nombre: 'Mateo',
     apellido: 'Sanchez',
-    edad: 28
+    edad: 25
 }
 
 var mafe = {
@@ -16,6 +16,21 @@ function imprimirNombreMayusculas({ nombre }) {
     console.log(nombre.toUpperCase());
 }
 
-imprimirNombreMayusculas(mateo);
-imprimirNombreMayusculas(mafe);
-imprimirNombreMayusculas({ nombre: 'Pepito' });
+/*Desestructurar objetos*/
+function imprimirNombreMayusculas2(persona) {
+    var { nombre } = persona;
+    console.log(nombre.toLowerCase());
+}
+
+function imprimirNombreYedad(persona) {
+    var { nombre } = persona;
+    var { edad } = persona;
+    console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
+}
+
+/*imprimirNombreMayusculas(mateo);
+imprimirNombreMayusculas2(mafe);
+imprimirNombreMayusculas({ nombre: 'Pepito' });*/
+
+imprimirNombreYedad(mateo);
+imprimirNombreYedad(mafe);

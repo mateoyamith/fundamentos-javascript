35b806522e560801c764a89ce60d81c1e70d4250
mateoyamith <mateosanchez9602@gmail.com>
2021-02-03 20:27:06 -0500

    Estructuras repetitivas: while

--- scripts/for.js
+++ scripts/for.js
@@ -7,10 +7,28 @@ var mateo = {
 
 console.log(`Al inicio del año ${mateo.nombre} pesa ${mateo.peso}kg`);
 
-const INCREMENTO_PESO = 0.2;
+const INCREMENTO_PESO = 0.3;
 const aumentarPeso = (persona) => persona.peso += INCREMENTO_PESO;
 const disminuirPeso = (persona) => persona.peso -= INCREMENTO_PESO;
+const comeMucho = () => Math.random() < 0.3;
+const realizaDeporte = () => Math.random() < 0.4;
 
+const META = mateo.peso - 3;
+var dias = 0;
+
+while (mateo.peso > META) {
+    if (comeMucho()) {
+        aumentarPeso(mateo);
+    }
+    if (realizaDeporte()) {
+        disminuirPeso(mateo);
+    }
+    dias++;
+}
+
+console.log(`Pasaron ${dias} días hasta que ${mateo.nombre} adelgazo 3kg`);
+
+/*
 for (let index = 1; index <= 365; index++) {
     var random = Math.random();
     if (random < 0.25) {
@@ -21,3 +39,4 @@ for (let index = 1; index <= 365; index++) {
 }
 
 console.log(`Al final del año ${mateo.nombre} pesa ${mateo.peso.toFixed(2)}kg`);
+*/

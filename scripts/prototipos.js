68c265916910cd2a09f92150a0d2a337ff81084b
mateoyamith <mateosanchez9602@gmail.com>
2021-02-10 21:12:23 -0500

    La verdad oculta sobre las clases en JavaScript

--- scripts/prototipos.js
+++ scripts/prototipos.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function() {}
+    fn.prototype = prototipoPadre.prototype;
+    prototipoHijo.prototype = new fn;
+    prototipoHijo.prototype.constructor = prototipoHijo;
+}
+
 function Persona(nombre, apellido, altura) {
     this.nombre = nombre;
     this.apellido = apellido;
@@ -12,9 +19,23 @@ Persona.prototype.soyAlto = function() {
         console.log(`Soy ${this.altura >= 170 ? `alto`: `bajo`}`);
 }
 
-var mateo = new Persona('Mateo', 'Sanchez', 170);
-var mafe = new Persona('Mafe', 'Gallego', 158);
-mateo.saludar();
-mateo.soyAlto();
-mafe.saludar();
-mafe.soyAlto();
+function Desarrollador(nombre, apellido){
+this.nombre = nombre;
+this.apellido = apellido;
+}
+
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`);
+}
+
+
+
+// var mateo = new Persona('Mateo', 'Sanchez', 170);
+// var mafe = new Persona('Mafe', 'Gallego', 158);
+// mateo.saludar();
+// mateo.soyAlto();
+// mafe.saludar();
+// mafe.soyAlto();
+// var mafe = new Desarrollador('Mafe', 'Gallego', 158);
